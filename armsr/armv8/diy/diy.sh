9acc26923b54825470ede45f3ee1fe59f40080c6
Frank Zhang <rgb360@126.com>
2026-04-16 10:23:52 +0800

    chore: remove SSH action and tune N1 build profile

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -4,12 +4,13 @@
 patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Clone packages
-# git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 git clone https://github.com/QiuSimons/luci-app-daed clone/daed
 git clone https://github.com/sbwml/luci-app-mosdns clone/mosdns
+
+# git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 # git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
 
-# Adjust packages
+# Adjust packages（注意顺序）
 rm -rf feeds/luci/applications/luci-app-daed
 cp -rf clone/daed/luci-app-daed feeds/luci/applications/
 rm -rf feeds/packages/net/mosdns
