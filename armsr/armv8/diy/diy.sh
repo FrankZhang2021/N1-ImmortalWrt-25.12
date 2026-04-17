07efd76b8c28d801d14b143843e9a37a5ac11a25
Frank Zhang <rgb360@126.com>
2026-04-17 15:29:35 +0800

    fix(n1): align package config and feed customization order
    - Move diy.sh execution after second feeds update to prevent mosdns
      override from being reverted
    - Replace daed with passwall+ttyd+arpbind+nlbwmon+sqm+statistics
    - Add luci-theme-argon; disable design/netgear themes
    - Remove turboacc (not available in openwrt-25.12 feeds)
    - Disable daed by default in .config; comment out daed ops in diy.sh
    - Add mosdns luci-app and v2dat copy steps; restore rm before cp
    - Update README: note daed is disabled by default

--- armsr/armv8/diy/diy.sh
+++ armsr/armv8/diy/diy.sh
@@ -4,17 +4,17 @@
 patch -p1 -f < $(dirname "$0")/luci.patch
 
 # Clone packages
-git clone https://github.com/QiuSimons/luci-app-daed clone/daed
 git clone https://github.com/sbwml/luci-app-mosdns clone/mosdns
 
+# git clone https://github.com/QiuSimons/luci-app-daed clone/daed
 # git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 # git clone https://github.com/Zerogiven-OpenWRT-Packages/luci-app-podman --depth=1 feeds/luci/applications/luci-app-podman
 
 # Adjust packages（注意顺序）
-rm -rf feeds/luci/applications/luci-app-daed
-cp -rf clone/daed/luci-app-daed feeds/luci/applications/
 rm -rf feeds/packages/net/mosdns
 cp -rf clone/mosdns/mosdns feeds/packages/net/
+cp -rf clone/mosdns/luci-app-mosdns feeds/luci/applications/
+cp -rf clone/mosdns/v2dat feeds/packages/net/
 sed -i '/luci-app-attendedsysupgrade/d' feeds/luci/collections/luci/Makefile
 
 # Clean packages
